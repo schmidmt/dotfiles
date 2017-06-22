0d8b7dc2bf2233f56f20ce748d141e0f331b69de
Mike Schmidt <schmidmt@gmail.com>
2017-06-22 16:01:58 -0500

    Fixed color commands in bashrc.d

--- bash/.bashrc.d/color.sh
+++ bash/.bashrc.d/color.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-if [ "$(uname -s)" == "Linux" ]; then
-	alias ls='ls --color=auto --classify'
-	alias grep='grep --color=auto'
-fi
+# Handle Pretty colored options for different cli tools
+
+case "$(uname -s)" in
+	Linux)
+		alias ls='ls --color=auto --classify'
+		alias grep='grep --color=auto'
+		;;
+	Darwin)
+		if which gls >/dev/null; then
+			alias ls="gls --color=auto --classify"
+			alias grep="ggrep --color=auto"
+			alias find="gfind"
+		fi
+		;;
+esac
